f132eb17f2246f109fbfe589d4e4c239c7334572
Riken Shah <rikenshah.02@gmail.com>
2021-07-31 15:08:41 +0530

    Add a module to connect with GCP.

---
+++ cloud/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const Cloud = require("@google-cloud/storage");
+const path = require("path");
+const serviceKey = path.join(__dirname, "./keys.json");
+
+const { Storage } = Cloud;
+const storage = new Storage({
+    keyFilename: serviceKey,
+    projectId: process.env.GCP_PROJECT_ID,
+});
+
+module.exports = storage;
